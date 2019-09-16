3c6e6729741ae715460b618937db8e37b1bc456e
huyifan01 <yifan_hu@zju.edu.cn>
2019-09-16 16:57:14 +0800

    feat: 搭建代码架构

---
+++ daiClient/public/static/js/constant.js
@@ -0,0 +1,19 @@
+function getAddress(){
+    return  "http://212.64.85.208:9092";
+}
+
+function getIpfsAddres(){
+    // ipfs hash
+    return "http://212.64.85.208:9092";
+}
+
+function getMonitorServer() {
+    // monitor server
+    return "http://212.64.85.208:9092";
+}
+
+function getTrainServer() {
+    // train
+    return "http://212.64.85.208:9092";
+}
+
